6f7cd94dbbeb26dbaf58537d17b927c9c9969636
korrakot-impact360 <korrakot.intanon@impact360design.com>
2024-04-14 01:44:33 +0700

    shuffle extension

--- Assets/ExtensionMethods.cs
+++ Assets/ExtensionMethods.cs
@@ -1,3 +1,6 @@
+
+using System.Collections.Generic;
+using UnityEngine;
 public static class ExtensionMethods
 {
 
@@ -6,4 +9,16 @@ public static class ExtensionMethods
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 
+    public static void Shuffle<T>(this List<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 }
